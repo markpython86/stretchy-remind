bcadfbb1a510b5dffacbc316fd8423f7d8296c04
Mark <mark.faraj86@gmail.com>
2020-04-12 16:30:26 -0500

    finalizing the app

--- StretchRemind.py
+++ StretchRemind.py
@@ -20,7 +20,7 @@ def stretchy_truthy(win, more, ee1,ee2):
     if more:
         win.destroy()
         if ee1 and ee2:
-            snooze = int(ee1)
+            snooze = int(ee1) * 60
             stretch_time = int(ee2)
             print(ee1)
             print(ee2)
@@ -73,27 +73,32 @@ def StretchRemindMain(more, stretchTime):
     y = (win.winfo_screenheight() + win.winfo_reqheight()) / 2
     win.geometry("+%d+%d" % (x, y))
     win.deiconify()
+    win.iconbitmap('assets/img/icon.ico')
     # win = root
     win.title('Stretchy Time!!')
     message1 = 'Time for Stretch Bud!'
-    message2 = 'Current Snooze time={0} seconds'.format(snooze)
+    message2 = 'Current Snooze Time is {0} Minutes'.format(snooze)
     message3 = 'Do you wanna continue?'
     if not more:
         message1 = 'Stretch reminder cos your a busy wolf!'
+        message2 = ''
         message3 = 'Do you wanna Start?'
     ttk.Label(win, text=' ').grid(column=0, row=0)
     ttk.Label(win, text=message1, font=("Times Bold", 14)).grid(column=0, row=1, padx=(100, 100))
     ttk.Label(win, text=message2, font=("Times Bold", 10)).grid(column=0, row=2, padx=(100, 100))
     ttk.Label(win, text=message3, font=("Times Bold", 10)).grid(column=0, row=3, padx=(100, 100))
     if not more:
+        ttk.Label(win, text='Snooze interval (Minutes):', font=("Times Bold", 10)).grid(column=0, row=4, sticky='w', padx=(100, 0), pady=(15, 15),ipadx=(5))
         e1 = ttk.Entry(win)
-        e1.grid(column=0, row=4, sticky='w', padx=(100, 0), pady=(15, 15),ipadx=(5))
+        e1.grid(column=0, row=4, padx=(150, 20), pady=(15, 15),ipadx=(5))
+        ttk.Label(win,text='Stretch interval (Seconds):', font=("Times Bold", 10)).grid(column=0, row=5, sticky='w', padx=(100, 0), pady=(15, 15),ipadx=(5))
         e2 = ttk.Entry(win)
-        e2.grid(column=0, row=4,sticky='e', padx=(0, 100), pady=(15, 15),ipadx=(5))
+        e2.grid(column=0, row=5, padx=(150, 20), pady=(15, 15),ipadx=(5))
         def ee1():
             val1 = e1.get()
             val2 = e2.get()
             try:
+                print(val1)
                 val1 = int(val1)
                 val2 = int(val2)
             except OSError as err:
@@ -101,7 +106,7 @@ def StretchRemindMain(more, stretchTime):
                 tkinter.messagebox.showwarning('Uh-Oh', 'Somthing went wrong, try to reopen the App')
             except ValueError:
                 print("Could not convert data to an integer.")
-                tkinter.messagebox.showwarning('I want Numbers', 'Sorry My Dude, those fields are for numbers only')
+                tkinter.messagebox.showwarning('I want Numbers', 'Sorry My Dude, you gotta enter a number in those fields')
             except:
                 print("Unexpected error:", sys.exc_info()[0])
                 tkinter.messagebox.showerror('Unexpected error', 'Please Restart the Application')
@@ -110,7 +115,7 @@ def StretchRemindMain(more, stretchTime):
                 print(val2)
                 stretchy_truthy(win, True, val1, val2)
         continue_btn = ttk.Button(win, text='Yes', command=lambda: ee1())
-        continue_btn.grid(column=0, row=5, padx=(100, 100), pady=(15, 15))
+        continue_btn.grid(column=0, row=6, padx=(100, 100), pady=(15, 15))
     if more:
         yes_btn = ttk.Button(win, text='Yes', command=lambda: stretchy_truthy(win, True, False, False))
         yes_btn.grid(column=0, row=4, sticky='w', padx=(100, 0), pady=(15, 15))
@@ -128,27 +133,12 @@ def StretchRemindMain(more, stretchTime):
         root.destroy()
     win.protocol("WM_DELETE_WINDOW", on_closing)
 
-
-print('\n\n\n')
-print('Welcome to Stretchy Time!')
-print('-------------------------------------------------')
-print('The app will keep running until you select no')
-print('-------------')
-# snooze = int(input('Enter Snooze interval:'))  # * 60
-# stretch_time = int(input('Enter Stretch Time interval (Seconds):'))
-print('\n\nThanks! You will get your first reminder in {0} seconds'.format(snooze))
-print('\n')
-print('Starting....')
-
 root = Tk()
 root.withdraw()
+root.iconbitmap('assets/img/icon.ico')
 
 timer = 1
 StretchRemindMain(False, stretch_time)
 
-
-
-#
-# root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
 print('Exiting, bye')
